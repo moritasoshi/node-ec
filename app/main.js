5a8c3a18e9fe15c545d485a70d1e96081faeb236
moritasoshi <soshi.morita@rakus-partners.co.jp>
2021-01-15 15:26:29 +0900

    ログイン/ログアウト機能の実装

--- app/main.js
+++ app/main.js
@@ -5,8 +5,14 @@ const express = require('express'),
 	router = require("./routes/index"),
 	bodyParser = require('body-parser'),
 	mongoose = require("mongoose"),
+	passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy,
+	session = require('express-session'),
+	flash = require('connect-flash'),
+	User = require("./models/user"),
 	PORT = 3000,
 	app = express();
+
 
 // DB
 mongoose
@@ -27,10 +33,66 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+// 認証
+app.use(passport.initialize());
+app.use(flash());
+passport.use(new LocalStrategy({
+		usernameField: 'email',
+	},
+	function (username, password, done) {
+		User.findOne({email: username}, function (err, user) {
+			if (err) {
+				return done(err);
+			}
+			if (!user) {
+				console.log("ユーザーIDが正しくありません")
+				return done(null, false, {message: 'ユーザーIDが正しくありません。'});
+			}
+			if (user.password !== password) {
+				console.log("パスワードが正しくありません")
+				return done(null, false, {message: 'パスワードが正しくありません。'});
+			}
+			return done(null, user);
+		});
+	}
+));
+
+// セッション管理
+app.use(session({
+	secret: 'keyboard cat',
+	resave: true,
+	saveUninitialized: false,
+}));
+passport.serializeUser((user, done) => {
+	console.log('Serialize ...');
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	console.log('Deserialize ...');
+	done(null, user);
+});
+
+
+app.post('/account/login',
+	passport.authenticate('local', {
+		failureRedirect: "/account/login",
+		failureFlash: true,
+		successRedirect: "/",
+		// successFlash: "Logged in!"
+	})
+);
+
 // ルーティング
 app.use("/", router);
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	if (!req.session.passport || !req.session.passport.user) {
+		console.log("login user : none")
+	} else {
+		console.log("login user : " + req.session.passport.user.email)
+	}
+	console.log(req.session);
+	res.render("./sample.ejs", {user: req.user});
 });
 
 // サーバー
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router(),
-	userController = require("../controllers/userController")
+	userController = require("../controllers/userController");
 
 router.get("/register", userController.toRegister);
 router.post("/register", userController.register, userController.toLogin);
+router.get("/login", userController.toLogin);
+router.get("/logout", userController.logout);
 
 module.exports = router;
