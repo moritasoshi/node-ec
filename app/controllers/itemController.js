8653bd619a9b34c589795a1f74502c96ce8f11bb
moritasoshi <soshi.morita@rakus-partners.co.jp>
2021-01-20 15:46:21 +0900

    検索、絞り込み、並び替え機能を実装

--- app/controllers/itemController.js
+++ app/controllers/itemController.js
@@ -30,17 +30,49 @@ module.exports = {
 	},
 	itemList: async (req, res) => {
 		const query = req.query;
-		console.log(query.name)
+		// カテゴリー一覧を取得
+		let categoryList = await Item
+			.find({}, {"_id": 0, "category": 1}, (err, data) => {
+				if (err) throw err;
+				return data;
+			});
+		categoryList = categoryList.map(v => v.category);
 
-		// 検索条件を生成
-		let filter = {}
+		// 検索条件, ソート条件を生成
+		const filter = {
+			$and: [
+				{
+					$or: [
+						{name: new RegExp(query.name)},
+						{description: new RegExp(query.name)},
+					]
+				},
+				{category: new RegExp(query.category)},
+			]
+		}
+		const sortGenerate = function (order) {
+			if (order === "1") return {releaseDate: -1};
+			if (order === "2") return {price: 1};
+			if (order === "3") return {price: -1};
+			return {};
+		}
+		const sort = sortGenerate(query.order);
 
-		const itemList = await Item.find(filter, (err, data) => {
-			if (err) throw err;
-			return data;
-		});
+		// 商品検索
+		const itemList = await Item
+			.find(filter)
+			.sort(sort)
+			.then(data => {
+				return data
+			})
+			.catch(err => {
+				throw err
+			});
+
 		const locals = {
-			itemList: itemList
+			itemList: itemList,
+			categoryList: categoryList,
+			originals: query
 		}
 		res.render("./item/list.ejs", locals);
 	}
