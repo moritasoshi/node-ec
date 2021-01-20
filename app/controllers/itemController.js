e7ca2f91243ee27ef3e2c2c87d90dbc9956c603c
etokohei <kohei.eto@rakus-partners.co.jp>
2021-01-20 10:07:04 +0900

    商品詳細画面表示処理の修正

--- app/controllers/itemController.js
+++ app/controllers/itemController.js
@@ -15,28 +15,15 @@ const Item = require("../models/item"),
 	};
 
 
-<<<<<<< HEAD
-    Item.find({_id: "60011c2e19a23bdda3b07d7c"}, function(err, result) {
-      if (err) throw err;
-      console.log(result[0]._id);
-      res.render('./detail.ejs', {
-        _id: result[0]._id,
-        name: result[0].name,
-        price: result[0].price,
-        photoURL: result[0].photoURL,
-        description: result[0].description,
-      });
-      
-      
-    });
-=======
 module.exports = {
+
+
 	show: (req, res) => {
 		Item.find({_id: "60011c2e19a23bdda3b07d7c"}, function (err, result) {
 			if (err) throw err;
->>>>>>> d3124351446df028059a8cf4b8b49ff9c830fb86
 
 			res.render('./detail.ejs', {
+        _id: result[0]._id,
 				name: result[0].name,
 				price: result[0].price,
 				photoURL: result[0].photoURL,
