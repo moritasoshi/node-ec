7a043accb1e5063cde043e53c647c2f24038c0cc
etokohei <kohei.eto@rakus-partners.co.jp>
2021-01-27 17:14:20 +0900

    注文完了処理修正

--- app/controllers/orderController.js
+++ app/controllers/orderController.js
@@ -20,74 +20,59 @@ const Order = require("../models/order"),
 module.exports = {
 
    //カート中身画面表示
-   
 	 index: async(req, res) => {
-    
-     var cart;
-     
-     //カートなし
-     const order = await Order.find({}, function(err, orderResult) {
-       if (err) throw err;
-       return orderResult;
-    });
 
-
-      //カートなし
-      if (!order) {
-        cart = 1;   
-      }
-        
-      //カートあり
-      if (order) {
-        cart = 2;
-      }
-    
-     //カートなし
-     /* if (cart = 1) {
-        res.render('./cart.ejs', {
-          noCart: 'カートに商品がありません',
-          body: [],
-          total: [],
-          //cartIn: '',
-        });
-      } */
-      
-      //カート商品あり
-      if (cart = 2) {
         //orderを取得
-        const newOrder = await Order.findOne({}, function(err, result) {
+        const newOrder = await Order.find({ status: 0 }, function(err, result) {
           if (err) throw err;
           return result;
         });
-        
-        //orderItemの数だけorderItemとitemをjoinして取得  
-        await OrderItem.find({_id : newOrder.orderItems})
-          .populate("item")
-          .exec(function(err, orderItemResult) {
-            if (err) throw err;           
-            //return orderItemResult;
-            //金額算出
-            var total = 0;
-            orderItemResult.forEach(orderItem => {
-              var nowTotal;
-              nowTotal = orderItem.quantity * orderItem.item.price;
-              total = total + nowTotal;
-            });
-            //orderのsubtotalを更新
-            Order.findByIdAndUpdate(newOrder._id,{
-              $set: { subTotal: total } 
-            }).catch(err => {
-              console.error(err);
-              throw err;
+        //console.log(newOrder);
+        if (newOrder != []) {
+          //orderItemの数だけorderItemとitemをjoinして取得  
+        await OrderItem.find({ _id: newOrder[0].orderItems })
+        .populate("item")
+        .exec(function(err, orderItemResult) {
+          if (err) throw err;           
+          //return orderItemResult;
+          //金額算出
+          var total = 0;
+          //console.log(orderItemResult);
+          orderItemResult.forEach(orderItem => {
+            //console.log('kkkkkkkk');
+            //console.log(orderItem);
+            var nowTotal;
+            nowTotal = orderItem.quantity * orderItem.item.price;
+            total = total + nowTotal;
+          });
+          //orderのsubtotalを更新
+          /* Order.findByIdAndUpdate(newOrder[0]._id,{
+            $set: { subTotal: total } 
+          }).catch(err => {
+            console.error(err);
+            throw err;
+          }) */
+          Order.findByIdAndUpdate(newOrder[0]._id, {
+              $set: {subTotal: total} },
+            function() {
+              if (err) throw err; 
             })
             res.render('./cart.ejs',{
-              //cartIn: '',
-              //noCart: '',
-              orderItemResult : orderItemResult,
-              total : total,
-            });   
-          });
-      }
+            orderItemResult : orderItemResult,
+            total : total,
+            user: req.user,
+          });   
+        });
+
+        } else if (newOrder == []) {
+          res.render('./cart.ejs',{
+            //orderItemResult : orderItemResult,
+            total : total,
+            user: req.user,
+          });   
+        }
+        
+      
 　},
 
   //カート数量変更
@@ -168,7 +153,7 @@ module.exports = {
 		const newOrderItem = getOrderItemParams(req.body);
 
 		// カートがすでに存在するか判定
-		let order = await Order.findOne({user: loginUser._id})
+		let order = await Order.findOne({ status: 0 },{user: loginUser._id})
 			.populate("orderItems")
 			.exec()
 			.then(data => {
@@ -179,7 +164,8 @@ module.exports = {
 				throw err;
       });
       
-      
+    //console.log('llllll');
+    //console.log(req.user);
 		if (!order) { // カートなしの場合は新規作成
 			const newOrder = new Order({
 				user: loginUser._id,
@@ -240,8 +226,11 @@ module.exports = {
   
   //注文確認画面表示
   confirm: async (req, res) => {
-    //orderを取得
-    await Order.find({})
+    //orderを指定して取得
+    //onsole.log(req.user);
+    //console.log('kkkkkkk');
+    await Order.find({ user: req.user._id },
+                     { status: 0 })
       .populate("user")
       .exec(function(err, orderResult) {
         if (err) throw err;
@@ -262,6 +251,10 @@ module.exports = {
               .exec(function(err, addressResult) {
                 if (err) throw err;
                 //console.log(addressResult[0].defaultAddress);
+
+                console.log(orderResult[0].subTotal);
+                console.log(total);
+                console.log(tax);
                 
                 
 
@@ -281,28 +274,37 @@ module.exports = {
           })
         
       })
-
+    
+    
   },
 
   //注文確定する
   determine: async (req, res) => {
-    
+
+    //orderを指定して取得
+    const determineOrder = Order.find({ user: req.user._id },
+               { status: 0 },
+               function(err, result) {
+                if (err) throw err;
+                
+               
+              
     //orderを更新
-    Order.update(
+    Order.findByIdAndUpdate(result[0]._id,
       { $set: { subTotal: req.body.subTotal,
                 tax: req.body.tax,
-                total: req.body.total,
+          　    total: req.body.total,
                 orderDate: new Date(),
                 destinationAddress: req.body.address,
                 paymentMethod: req.body.pay,
                 status: 1,
-                 } },
+              }},
       function(err) {
         if (err) throw err;
-      }
-    )
+      })
+
     res.render("./orderFinished.ejs", {    
     })
+  })
   },
-
 }
