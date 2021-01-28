49c8b5a5d6870207d858b017628036af843fbda1
moritasoshi <soshi.morita@rakus-partners.co.jp>
2021-01-28 12:52:37 +0900

    ライブラリをdevに移動＆コメント追加

--- app/__tests__/sample.test.js
+++ app/__tests__/sample.test.js
@@ -4,6 +4,7 @@ describe('insert', () => {
 	let connection;
 	let db;
 
+	// DB接続
 	beforeAll(async () => {
 		connection = await MongoClient.connect("mongodb://localhost/nodec_test", {
 			useNewUrlParser: true,
@@ -12,20 +13,23 @@ describe('insert', () => {
 		db = await connection.db();
 	});
 
-	afterEach(async () => {
+	// テスト前にドキュメントを削除
+	beforeEach(async () => {
 		await db.collection("users").deleteMany({});
 	});
 
+	// テスト
 	it('should insert a doc into collection', async () => {
 		const users = db.collection('users');
 
 		const mockUser = {_id: 'some-user-id', name: 'John'};
-		await users.insertOne(mockUser);
+		await users.insertOne(mockUser); // insert
 
-		const insertedUser = await users.findOne({_id: 'some-user-id'});
-		expect(insertedUser).toEqual(mockUser);
+		const insertedUser = await users.findOne({_id: 'some-user-id'}); // find
+		expect(insertedUser).toEqual(mockUser);  // insertしたデータとfindしたデータの一致を確認
 	});
 
+	// DB切断
 	afterAll(async () => {
 		await connection.close();
 	});
