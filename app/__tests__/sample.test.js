d2b21abb49ba7d83bca295390acb3e1c3550be5e
moritasoshi <soshi.morita@rakus-partners.co.jp>
2021-01-28 14:25:25 +0900

    テスト用のDBライブラリをmongooseに変更

--- app/__tests__/sample.test.js
+++ app/__tests__/sample.test.js
@@ -1,37 +1,61 @@
-const {MongoClient} = require('mongodb');
+const mongoose = require("mongoose"),
+	Item = require("../models/item"),
+	itemController = require('../controllers/itemController');
 
 describe('insert', () => {
-	let connection;
-	let db;
 
 	// DB接続
 	beforeAll(async () => {
-		connection = await MongoClient.connect("mongodb://localhost/nodec_test", {
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-		});
-		db = await connection.db();
+		await mongoose
+			// .connect('mongodb://localhost:30001,localhost:30002,localhost:30003/nodec?replicaSet=rs0', {
+			.connect('mongodb://localhost/nodec_test', {
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useFindAndModify: false
+			})
 	});
 
 	// テスト前にドキュメントを削除
 	beforeEach(async () => {
-		await db.collection("users").deleteMany({});
+		await Item.deleteMany({});
 	});
 
 	// テスト
-	it('should insert a doc into collection', async () => {
-		const users = db.collection('users');
+	it('should insert a doc into collection2', async () => {
+		// データの追加
+		const mockItem = {
+			"name": "天皇の国史",
+			"price": 1980,
+			"releaseDate": "2020/08/13",
+			"category": "社会/政治",
+			"description": "これまで通史といえば、目まぐるしく交代する権力者を中心とした政治史が一般的だった。本書はそれとは異なり、二千年来変わることがなかった天皇を軸として、国史を取り纏めたものである。故に主題を『天皇の国史』としている",
+			"photoURL": "1.jpg"
+		};
 
-		const mockUser = {_id: 'some-user-id', name: 'John'};
-		await users.insertOne(mockUser); // insert
+		await Item.insertMany(mockItem); // insert
+		const insertedItem = await Item.findOne({name: '天皇の国史'});
 
-		const insertedUser = await users.findOne({_id: 'some-user-id'}); // find
-		expect(insertedUser).toEqual(mockUser);  // insertしたデータとfindしたデータの一致を確認
+		// モックの作成
+		const req = {
+			params: {
+				_id: insertedItem['_id']
+			}
+		};
+		const res = {
+			render: jest.fn()
+		};
+		await res.render.mockClear();
+
+		// テスト対象の呼び出し
+		await itemController.show(req, res)
+
+		// 確認
+		expect(res.render).toHaveBeenCalledWith('./detail.ejs', insertedItem);
 	});
 
 	// DB切断
 	afterAll(async () => {
-		await connection.close();
+		await mongoose.disconnect();
 	});
 });
 
